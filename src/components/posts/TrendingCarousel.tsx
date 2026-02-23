ef82c8158b0c86ce80c8a70a67f5aad6fa540282
Fredler21 <pierrelouisfredler@gmail.com>
2026-02-23 21:13:39 +0000

    fix: show scrollbar on trending carousel for mobile

--- src/components/posts/TrendingCarousel.tsx
+++ src/components/posts/TrendingCarousel.tsx
@@ -7,7 +7,7 @@ type TrendingCarouselProps = {
 
 export function TrendingCarousel({ posts }: TrendingCarouselProps) {
   return (
-    <div className="hide-scrollbar -mx-4 flex snap-x snap-mandatory gap-5 overflow-x-auto px-4 pb-4 sm:-mx-0 sm:px-0">
+    <div className="-mx-4 flex snap-x snap-mandatory gap-5 overflow-x-auto px-4 pb-4 sm:-mx-0 sm:px-0">
       {posts.map((post, i) => (
         <div key={post.slug} className="min-w-[300px] snap-start sm:min-w-[340px] lg:min-w-[360px]">
           <PostCard post={post} trending={i < 3} />
