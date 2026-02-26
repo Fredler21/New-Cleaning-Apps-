dffc995c08b32dd3112e7c2f99ed3d51e468c652
Fredler21 <pierrelouisfredler@gmail.com>
2026-02-26 19:33:18 +0000

    Show trending badge on all 5 trending posts

--- src/components/posts/TrendingCarousel.tsx
+++ src/components/posts/TrendingCarousel.tsx
@@ -10,7 +10,7 @@ export function TrendingCarousel({ posts }: TrendingCarouselProps) {
     <div className="-mx-4 flex snap-x snap-mandatory gap-5 overflow-x-auto px-4 pb-4 sm:-mx-0 sm:px-0">
       {posts.map((post, i) => (
         <div key={post.slug} className="min-w-[300px] snap-start sm:min-w-[340px] lg:min-w-[360px]">
-          <PostCard post={post} trending={i < 3} />
+          <PostCard post={post} trending={true} />
         </div>
       ))}
     </div>
