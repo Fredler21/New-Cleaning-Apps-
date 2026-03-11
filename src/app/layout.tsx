2d063dca9cbd0f4a907f60f1ff4630a4a27c3d83
Fredler21 <pierrelouisfredler@gmail.com>
2026-03-11 22:04:44 +0000

    Add Google Tag Manager (head + noscript body)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -75,6 +75,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           crossOrigin="anonymous"
         />
       </head>
+      <Script id="gtm" strategy="afterInteractive">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5PRLLC75');`}
+      </Script>
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-R97SKNX12S"
         strategy="afterInteractive"
@@ -99,6 +106,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <img height="1" width="1" style={{display:'none'}} alt="" src="https://ct.pinterest.com/v3/?event=init&tid=2613182917179&noscript=1" />
       </noscript>
       <body className="font-sans antialiased">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5PRLLC75"
+            height="0"
+            width="0"
+            style={{display:'none',visibility:'hidden'}}
+          />
+        </noscript>
         <ThemeProvider>
           <SavedHacksProvider>
             <JsonLd
