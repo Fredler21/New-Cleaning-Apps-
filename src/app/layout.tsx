109c25e34d729412c5e17bfe839697107c9addfa
Fredler21 <pierrelouisfredler@gmail.com>
2026-03-11 21:57:13 +0000

    Add Pinterest tracking tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -87,6 +87,17 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           gtag('config', 'G-R97SKNX12S');
         `}
       </Script>
+      <Script id="pinterest-tag" strategy="afterInteractive">
+        {`
+          !function(e){if(!window.pintrk){window.pintrk=function(){window.pintrk.queue.push(Array.prototype.slice.call(arguments))};var n=window.pintrk;n.queue=[],n.version="3.0";var t=document.createElement("script");t.async=!0,t.src=e;var r=document.getElementsByTagName("script")[0];r.parentNode.insertBefore(t,r)}}("https://s.pinimg.com/ct/core.js");
+          pintrk('load', '2613182917179');
+          pintrk('page');
+        `}
+      </Script>
+      <noscript>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img height="1" width="1" style={{display:'none'}} alt="" src="https://ct.pinterest.com/v3/?event=init&tid=2613182917179&noscript=1" />
+      </noscript>
       <body className="font-sans antialiased">
         <ThemeProvider>
           <SavedHacksProvider>
